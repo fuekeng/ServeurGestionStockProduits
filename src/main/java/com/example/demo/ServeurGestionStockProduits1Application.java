e07d3bc11c035071aea21d69a778f6cf96a86dfe
fuekeng <fuekengbilly@gmail.com>
2020-08-05 15:10:09 +0100

    mon premier commit du serveur

---
+++ src/main/java/com/example/demo/ServeurGestionStockProduits1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProduits1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProduits1Application.class, args);
+	}
+
+}
